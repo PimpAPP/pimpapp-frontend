cc8e97c3ba40a71f88187738e3367ca47c44523a
Katrine Tjoelsen <ktjolsen@mit.edu>
2016-01-11 15:50:03 -0200

    Moved page templates into separate files. Added elements to registration form. Created method to insert into database on the server end instead of client side.

--- registrations.js
+++ registrations.js
@@ -5,14 +5,31 @@ if (Meteor.isClient) {
   Template.addCarroceiroForm.events({
     'submit form': function(event) {
       event.preventDefault();
+
+      // retrieve form data
       var carroceiroName = event.target.carroceiroName.value;
       var carroceiroLocation = event.target.desiredAddress.value;
+      var carroceiroTelephone = event.target.carroceiroTelephone.value;
+      
+      // TODO: retrieve tipo de servicos
+      // TODO: retrieve outra informacao
+
       console.log('Form submitted. Carroceiro name: ' + carroceiroName + ', Location: ' + carroceiroLocation);
 
-      CarroceiroData.insert({
-        'name': carroceiroName,
-        'location': carroceiroLocation
-      });
+      Meteor.call('insertCarroceiro', carroceiroName, carroceiroLocation, carroceiroTelephone);
+      
+      // clear form
+      document.getElementById("carroceiroRegistrationForm").reset();
     }
   });
 }
+
+Meteor.methods({
+  'insertCarroceiro': function(name, address, telephone) {
+    CarroceiroData.insert({
+      'name': name,
+      'address': address,
+      'telephone': telephone
+    });
+  }
+});
