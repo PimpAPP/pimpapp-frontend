cf1509d55cd31d44ec52f7ab4c16c11df0f2d2ca
Katrine Tjoelsen <ktjolsen@mit.edu>
2016-01-19 10:47:44 -0200

    Started improving layout of profile

--- client/helpers/profile_catador.js
+++ client/helpers/profile_catador.js
@@ -13,7 +13,7 @@ Template.imageView.helpers({
 });
 
 
-Template.CatadorDetails.helpers({
+Template.PersonalDetails.helpers({
   
   name: function() {
     var catador = Catadores.findOne(getCatadorIdFromUrl());
@@ -34,37 +34,38 @@ Template.CatadorDetails.helpers({
     var catador = Catadores.findOne(getCatadorIdFromUrl());
     var returnVal = (catador.whatsapp)? 'Sim' : 'Não';
     return returnVal;
-  },
-
-  address: function() {
-    var catador = Catadores.findOne(getCatadorIdFromUrl());
-    return catador.address.fullAddress;
-  },
-
-  motorizedVehicle: function() {
-    var catador = Catadores.findOne(getCatadorIdFromUrl());
-    var returnVal = (catador.motorizedVehicle)? 'Sim' : 'Não';
-    return returnVal;
-  },
-
-  services: function() {
-    var catador = Catadores.findOne(getCatadorIdFromUrl());
-    var services = [];
-    if (catador.services_recyclable) services.push(services_recyclable_str);
-    if (catador.services_construction) services.push(services_construction_str);
-    if (catador.services_freight) services.push(services_freight_str);
-    if (catador.services_furniture) services.push(services_furniture_str);
-    if (catador.services_metals) services.push(services_metals_str);
-    if (catador.services_electronics) services.push(services_electronics_str);
-    if (catador.services_other_materials) services.push(services_other_materials_str);
-    if (catador.services_construction) services.push(services_construction_str);
-    if (catador.services_metals) services.push(services_metals_str);
-
-    var servicesString = services.join(", ");
-    
-    return servicesString;
   }
+
+  // address: function() {
+  //   var catador = Catadores.findOne(getCatadorIdFromUrl());
+  //   return catador.address.fullAddress;
+  // },
+
+  // motorizedVehicle: function() {
+  //   var catador = Catadores.findOne(getCatadorIdFromUrl());
+  //   var returnVal = (catador.motorizedVehicle)? 'Sim' : 'Não';
+  //   return returnVal;
+  // },
+
+  // services: function() {
+  //   var catador = Catadores.findOne(getCatadorIdFromUrl());
+  //   var services = [];
+  //   if (catador.services_recyclable) services.push(services_recyclable_str);
+  //   if (catador.services_construction) services.push(services_construction_str);
+  //   if (catador.services_freight) services.push(services_freight_str);
+  //   if (catador.services_furniture) services.push(services_furniture_str);
+  //   if (catador.services_metals) services.push(services_metals_str);
+  //   if (catador.services_electronics) services.push(services_electronics_str);
+  //   if (catador.services_other_materials) services.push(services_other_materials_str);
+  //   if (catador.services_construction) services.push(services_construction_str);
+  //   if (catador.services_metals) services.push(services_metals_str);
+
+  //   var servicesString = services.join(", ");
+    
+  //   return servicesString;
+  // }
 });
+
 
 
 // TODO: Would be better if this was done only once when page is loaded
