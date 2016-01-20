87ed544d01fc59e6902da8c32d402e4ae41ce012
Katrine Tjoelsen <ktjolsen@mit.edu>
2016-01-20 11:15:50 -0200

    Created cleaner profile layout for catadores.

--- client/helpers/profile_catador.js
+++ client/helpers/profile_catador.js
@@ -13,7 +13,7 @@ Template.imageView.helpers({
 });
 
 
-Template.PersonalDetails.helpers({
+Template.catadorprofile.helpers({
   
   name: function() {
     var catador = Catadores.findOne(getCatadorIdFromUrl());
@@ -34,37 +34,40 @@ Template.PersonalDetails.helpers({
     var catador = Catadores.findOne(getCatadorIdFromUrl());
     var returnVal = (catador.whatsapp)? 'Sim' : 'Não';
     return returnVal;
-  }
+  },
 
-  // address: function() {
-  //   var catador = Catadores.findOne(getCatadorIdFromUrl());
-  //   return catador.address.fullAddress;
-  // },
+  address: function() {
+    var catador = Catadores.findOne(getCatadorIdFromUrl());
+    return catador.address.fullAddress;
+  },
 
-  // motorizedVehicle: function() {
-  //   var catador = Catadores.findOne(getCatadorIdFromUrl());
-  //   var returnVal = (catador.motorizedVehicle)? 'Sim' : 'Não';
-  //   return returnVal;
-  // },
+  motorizedVehicle: function() {
+    var catador = Catadores.findOne(getCatadorIdFromUrl());
+    var returnVal = (catador.motorizedVehicle)? 'Sim' : 'Não';
+    return returnVal;
+  },
 
-  // services: function() {
-  //   var catador = Catadores.findOne(getCatadorIdFromUrl());
-  //   var services = [];
-  //   if (catador.services_recyclable) services.push(services_recyclable_str);
-  //   if (catador.services_construction) services.push(services_construction_str);
-  //   if (catador.services_freight) services.push(services_freight_str);
-  //   if (catador.services_furniture) services.push(services_furniture_str);
-  //   if (catador.services_metals) services.push(services_metals_str);
-  //   if (catador.services_electronics) services.push(services_electronics_str);
-  //   if (catador.services_other_materials) services.push(services_other_materials_str);
-  //   if (catador.services_construction) services.push(services_construction_str);
-  //   if (catador.services_metals) services.push(services_metals_str);
+  services: function() {
+    var catador = Catadores.findOne(getCatadorIdFromUrl());
+    var services = [];
+    if (catador.services_recyclable) services.push(services_recyclable_str);
+    if (catador.services_construction) services.push(services_construction_str);
+    if (catador.services_freight) services.push(services_freight_str);
+    if (catador.services_furniture) services.push(services_furniture_str);
+    if (catador.services_metals) services.push(services_metals_str);
+    if (catador.services_electronics) services.push(services_electronics_str);
+    if (catador.services_other_materials) services.push(services_other_materials_str);
+    if (catador.services_construction) services.push(services_construction_str);
+    if (catador.services_metals) services.push(services_metals_str);
 
-  //   var servicesString = services.join(", ");
+    var servicesString = services.join(", ");
     
-  //   return servicesString;
-  // }
+    return servicesString;
+  }  
+
 });
+
+
 
 
 
